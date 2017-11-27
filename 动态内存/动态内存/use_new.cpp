e6a7b076bd50c9bde1d52efa863901d270f24b52
Mr.Tang <2586557491@qq.com>
2017-11-27 15:03:24 +0800

    add use_new.cpp

---
+++ 动态内存/动态内存/use_new.cpp
@@ -0,0 +1,62 @@
+#include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <new>
+using namespace std;
+
+//vector<int> *new_vector_int();                                          //使用new的版本
+//vector<int> *input(vector<int> *vec);
+//void print(vector<int> *vec);
+//
+//vector<int> *new_vector_int() {
+//	return new vector<int>;
+//}
+//
+//vector<int> *input(vector<int> *vec) {
+//	int i;
+//	while (cin >> i)
+//		vec->push_back(i);
+//	return vec;
+//}
+//
+//void print(vector<int> *vec) {
+//	for (auto i : *(vec))
+//		cout << i << endl;
+//	delete vec;
+//	vec = nullptr;
+//}
+//
+//int main() {
+//	print(input(new_vector_int()));
+//	
+//	system("pause");
+//	return 0;
+//}
+
+shared_ptr<vector<int>> new_shared_ptr_vector_int();                      //使用shared_ptr
+shared_ptr<vector<int>> input(vector<int> *vec);
+void print(shared_ptr<vector<int>> vec);
+
+shared_ptr<vector<int>> new_shared_ptr_vector_int() {
+	return make_shared<vector<int>>();
+}
+
+shared_ptr<vector<int>> input(shared_ptr<vector<int>> vec) {
+	int i;
+	while (cin >> i)
+		vec->push_back(i);
+	return vec;
+}
+
+void print(shared_ptr<vector<int>> vec) {
+	for (auto i : *(vec))
+		cout << i << endl;
+}
+
+int main() {
+	print(input(new_shared_ptr_vector_int()));
+
+	system("pause");
+	return 0;
+}
---
+++ 动态内存/动态内存/use_strBlob.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <string>
+//#include "StrBlob.h"
+//#include <vector>
+//
+//int main() {
+//	strBlob s1 = { {"const"},{"string"},{"vector"},{"catch"},{"mikumiku"},{"0.0"},{"compare"},{"identify"},{"iostream"},{"int"},{"float"},{"double"} };
+//	std::string s = "deque";
+//	std::cout << s1.size() << std::endl;
+//	const strBlob s2(s1);
+//	s1.back() = s;
+//	std::cout << s1.back() << std::endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
